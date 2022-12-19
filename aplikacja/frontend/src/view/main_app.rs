1b54f12d8599750ffd29a55ce7ab20d35db5023e
Sabotax <62028372+Sabotax@users.noreply.github.com>
2022-12-19 01:39:33 +0100

    changed AppMsg construction so we can handle both leftpanel and right panel msg

--- aplikacja/frontend/src/view/main_app.rs
+++ aplikacja/frontend/src/view/main_app.rs
@@ -2,79 +2,136 @@ use gloo_console::log;
 // use gloo::console;
 // use js_sys::{Date, WebAssembly::Table};
 use yew::{html, Component, Context, Html, classes};
+use std::marker::Copy;
 // use std::fmt::{self, Debug, Display};
 // Define the possible messages which can be sent to the component
 
-#[derive(Eq, PartialEq)]
-pub enum AppState {
-    Default,
+#[derive(Eq, PartialEq,Copy,Clone)]
+pub enum LeftState {
     Dane,
     Led,
     Table,
-    Settings
+    Settings,
+    Default
 }
-
-impl AppState {
-    fn to_string(&self)-> &str {
-        return match self {
-            AppState::Default => "Default",
-            AppState::Dane => "Dane",
-            AppState::Led => "Led",
-            AppState::Table => "Table",
-            AppState::Settings => "Settings"
-        }
-    }
+#[derive(Eq, PartialEq)]
+pub enum RightState {
+    Dane(DaneState),
+    Led(LedState),
+    Table(TableState),
+    Settings(SettingsState),
+    Default
 }
-
+#[derive(Eq, PartialEq)]
+pub enum DaneState {
+    GetHumidity
+}
+#[derive(Eq, PartialEq)]
+pub enum LedState {
+    SetSingle,
+    SetAll,
+    GetSingle,
+    GetAll
+}
+#[derive(Eq, PartialEq)]
+pub enum TableState {
+    GetRow
+}
+#[derive(Eq, PartialEq)]
+pub enum SettingsState {
+    SetIP
+}
+#[derive(Eq, PartialEq)]
+pub enum AppMsg {
+    Left(LeftState),
+    Right(RightState)
+}
+#[derive(Eq, PartialEq)]
 pub enum PanelState {
     Active,
     Inactive
 }
 
 pub struct App {
-    state: AppState
+    left_state: LeftState,
+    right_state: RightState,
 }
 
 impl Component for App {
-    type Message = AppState;
+    type Message = AppMsg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
-            state: AppState::Default
+            left_state: LeftState::Default,
+            right_state: RightState::Default
             }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         log!("ABC");
-        if msg == self.state {
-            self.state = AppState::Default;
-            true
+
+        if msg == AppMsg::Left(self.left_state) {
+            self.left_state = LeftState::Default;
+            self.right_state = RightState::Default;
+            return true
         }
-        else {
-            match msg {
-            AppState::Default => {
+
+        match msg {
+            AppMsg::Left(LeftState::Default) => {
                 //nigdy się nie wydarzy
-                self.state = AppState::Default;
+                false
+            }
+            AppMsg::Left(LeftState::Dane) => {
+                self.left_state = LeftState::Dane;
                 true
             }
-            AppState::Dane => {
-                self.state = AppState::Dane;
+            AppMsg::Left(LeftState::Led) => {
+                self.left_state = LeftState::Led;
                 true
             }
-            AppState::Led => {
-                self.state = AppState::Led;
+            AppMsg::Left(LeftState::Table) => {
+                self.left_state = LeftState::Table;
                 true
             }
-            AppState::Table => {
-                self.state = AppState::Table;
+            AppMsg::Left(LeftState::Settings) => {
+                self.left_state = LeftState::Settings;
                 true
             }
-            AppState::Settings => {
-                self.state = AppState::Settings;
+            AppMsg::Right(RightState::Default) => {
+                //nie wydarzy sie
+                false
+            }
+            AppMsg::Right(RightState::Dane(DaneState::GetHumidity)) => {
+                //TODO
                 true
             }
-        }}
+            AppMsg::Right(RightState::Led(LedState::SetAll)) => {
+                //TODO
+                true
+            }
+            AppMsg::Right(RightState::Led(LedState::SetSingle)) => {
+                //TODO
+                true
+            }
+            AppMsg::Right(RightState::Led(LedState::GetAll)) => {
+                //TODO
+                true
+            }
+            AppMsg::Right(RightState::Led(LedState::GetSingle)) => {
+                //TODO
+                true
+            }
+            AppMsg::Right(RightState::Table(TableState::GetRow)) => {
+                //TODO
+                true
+            }
+            AppMsg::Right(RightState::Settings(SettingsState::SetIP)) => {
+                //TODO
+                true
+            }
+        }
+        
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
@@ -83,67 +140,67 @@ impl Component for App {
                 <div id="mainLeft">
                     <div 
                     id="strona1_panel"
-                    class={classes!("panel", match self.state {
-                        AppState::Default => "panelInactive",
-                        AppState::Dane => "panelActive",
-                        AppState::Led => "panelInactive",
-                        AppState::Table => "panelInactive",
-                        AppState::Settings => "panelInactive"
+                    class={classes!("panel", match self.left_state {
+                        LeftState::Default => "panelInactive",
+                        LeftState::Dane => "panelActive",
+                        LeftState::Led => "panelInactive",
+                        LeftState::Table => "panelInactive",
+                        LeftState::Settings => "panelInactive"
                     })}
-                    onclick={ctx.link().callback(|_| AppState::Dane)}
+                    onclick={ctx.link().callback(|_| AppMsg::Left(LeftState::Dane))}
                     >  
                         <p>{"Panel Danych"}</p>
                     </div>
 
                     <div 
                     id="strona2_panel"
-                    class={classes!("panel", match self.state {
-                        AppState::Default => "panelInactive",
-                        AppState::Dane => "panelInactive",
-                        AppState::Led => "panelActive",
-                        AppState::Table => "panelInactive",
-                        AppState::Settings => "panelInactive"
+                    class={classes!("panel", match self.left_state {
+                        LeftState::Default => "panelInactive",
+                        LeftState::Dane => "panelInactive",
+                        LeftState::Led => "panelActive",
+                        LeftState::Table => "panelInactive",
+                        LeftState::Settings => "panelInactive"
                     })}
-                    onclick={ctx.link().callback(|_| AppState::Led)}
+                    onclick={ctx.link().callback(|_| AppMsg::Left(LeftState::Led))}
                     >  
                         <p>{"Panel Led"}</p>
                     </div>
 
                     <div 
                     id="strona3_panel"
-                    class={classes!("panel", match self.state {
-                        AppState::Default => "panelInactive",
-                        AppState::Dane => "panelInactive",
-                        AppState::Led => "panelInactive",
-                        AppState::Table => "panelActive",
-                        AppState::Settings => "panelInactive"
+                    class={classes!("panel", match self.left_state {
+                        LeftState::Default => "panelInactive",
+                        LeftState::Dane => "panelInactive",
+                        LeftState::Led => "panelInactive",
+                        LeftState::Table => "panelActive",
+                        LeftState::Settings => "panelInactive"
                     })}
-                    onclick={ctx.link().callback(|_| AppState::Table)}
+                    onclick={ctx.link().callback(|_| AppMsg::Left(LeftState::Table))}
                     >  
                         <p>{"Panel Tabeli"}</p>
                     </div>
 
                     <div 
                     id="strona4_panel"
-                    class={classes!("panel", match self.state {
-                        AppState::Default => "panelInactive",
-                        AppState::Dane => "panelInactive",
-                        AppState::Led => "panelInactive",
-                        AppState::Table => "panelInactive",
-                        AppState::Settings => "panelActive"
+                    class={classes!("panel", match self.left_state {
+                        LeftState::Default => "panelInactive",
+                        LeftState::Dane => "panelInactive",
+                        LeftState::Led => "panelInactive",
+                        LeftState::Table => "panelInactive",
+                        LeftState::Settings => "panelActive"
                     })}
-                    onclick={ctx.link().callback(|_| AppState::Settings)}
+                    onclick={ctx.link().callback(|_| AppMsg::Left(LeftState::Settings))}
                     >  
                         <p>{"Panel Ustawień"}</p>
                     </div>
                 </div>
                 <div id="mainRight">
-                    {match self.state{
-                        AppState::Default => "Default",
-                        AppState::Dane => "Dane",
-                        AppState::Led => "Led",
-                        AppState::Table => "Table",
-                        AppState::Settings => "Settings"
+                    {match self.left_state{
+                        LeftState::Default => "Default",
+                        LeftState::Dane => "Dane",
+                        LeftState::Led => "Led",
+                        LeftState::Table => "Table",
+                        LeftState::Settings => "Settings"
                     }}
                 </div>
             </div>
